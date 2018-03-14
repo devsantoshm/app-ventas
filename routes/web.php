a3c463092a0d4a2586d4b0d178df3e7977658e76
SantosHM <dosantoshm@gmail.com>
2018-03-14 16:48:58 -0500

    Controlador resource y requests

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Agrupar las rutas en una solta ruta de tipo resource
+Route::resource('almacen/categoria', 'CategoriaController');
