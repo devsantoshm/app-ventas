2f064249a6ef4c219bd36a43abdc9872aad89367
SantosHM <dosantoshm@gmail.com>
2018-07-10 19:37:55 -0500

    Registr compras o ingresos almacén part1

--- resources/views/compras/ingreso/create.blade.php
+++ resources/views/compras/ingreso/create.blade.php
@@ -2,7 +2,7 @@
 @section('contenido')
 	<div class="row">
 		<div class="col-sm-6 col-xs-12">
-			<h3>Nueva Proveedor</h3>
+			<h3>Nuevo Ingreso</h3>
 			@if (count($errors) > 0)
 			<div class="alert alert-danger">
 				<ul>
@@ -15,46 +15,54 @@
 		</div>
 	</div>
 	<!-- llama al resource categoriaController y con el metodo post llama al metodo store -->
-	{!! Form::open(['url' => 'compras/proveedor', 'method'=>'POST', 'autocomplete'=>'off']) !!}
+	{!! Form::open(['url' => 'compras/ingreso', 'method'=>'POST', 'autocomplete'=>'off']) !!}
 	<div class="row">
-		<div class="col-sm-6 col-xs-12">
+		<div class="col-xs-12">
 			<div class="form-group">
-				<label for="nombre">Nombre</label>
-				<input type="text" name="nombre" required value="{{ old('nombre') }}" class="form-control" placeholder="Nombre">
-			</div>
-		</div>
-		<div class="col-sm-6 col-xs-12">
-			<div class="form-group">
-				<label for="direccion">Dirección</label>
-				<input type="text" name="direccion" value="{{ old('direccion') }}" class="form-control" placeholder="Dirección">
-			</div>
-		</div>
-		<div class="col-sm-6 col-xs-12">
-			<div class="form-group">
-				<label>Documento</label>
-				<select name="tipo_documento" class="form-control">
-					<option value="DNI">DNI</option>
-					<option value="RUC">RUC</option>
-					<option value="PAS">PAS</option>
+				<label for="proveedor">Proveedor:</label>
+				<select name="idproveedor" id="idproveedor" class="form-control">
+					@foreach ($personas as $persona)
+						<option value="{{ $persona->idpersona }}">{{ $persona->nombre }}</option>
+					@endforeach
 				</select>
 			</div>
 		</div>
-		<div class="col-sm-6 col-xs-12">
+		<div class="col-sm-4 col-xs-12">
 			<div class="form-group">
-				<label for="num_documento">Número documento</label>
-				<input type="text" name="num_documento" required value="{{ old('num_documento') }}" class="form-control" placeholder="Documento">
+				<label for="direccion">Comprobante</label>
+				<select name="tipo_comprobante" class="form-control">
+					<option value="Boleta">Boleta</option>
+					<option value="Factura">Factura</option>
+					<option value="Ticket">Ticket</option>
+				</select>
 			</div>
 		</div>
-		<div class="col-sm-6 col-xs-12">
+		<div class="col-sm-3 col-xs-12">
 			<div class="form-group">
-				<label for="telefono">Teléfono</label>
-				<input type="text" name="telefono" value="{{ old('telefono') }}" class="form-control" placeholder="Teléfono">
+				<label for="serie_comprobante">Serie Comprobante</label>
+				<input type="text" name="serie_comprobante" value="{{ old('serie_comprobante') }}" class="form-control" placeholder="Serie comprobante">
 			</div>
 		</div>
-		<div class="col-sm-6 col-xs-12">
+		<div class="col-sm-3 col-xs-12">
 			<div class="form-group">
-				<label for="email">Email</label>
-				<input type="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="Email">
+				<label for="num_comprobante">Número Comprobante</label>
+				<input type="text" name="num_comprobante" required value="{{ old('num_comprobante') }}" class="form-control" placeholder="Número comprobante">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="panel panel-primary">
+			<div class="panel-body">
+				<div class="col-md-3 col-xs-12">
+					<div class="form-group">
+						<label for="proveedor">Artículo:</label>
+						<select name="pidarticulo" id="pidarticulo" class="form-control">
+							@foreach ($articulos as $articulo)
+								<option value="{{ $articulo->idarticulo }}">{{ $articulo->articulo }}</option>
+							@endforeach
+						</select>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="col-sm-6 col-xs-12">
